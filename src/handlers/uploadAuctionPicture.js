357fd6d89702f8140c5eef7ccded48576554257b
whoiskai <6267554+whoiskai@users.noreply.github.com>
2021-04-20 21:03:23 +0800

    chatper 71: up till uploading picture and validate ownership

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,38 @@
+import { getAuctionById } from "./getAuction";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
+
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(`You are not the seller of this auctions!`);
+  }
+
+
+  // strip off chars that don't play well with S3
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+
+  let updatedAuction;
+
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
